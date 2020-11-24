6497a1730c5dd68b8443727737bea0cb86a31d8c
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2020-11-23 21:09:54 -0500

    NodeJS Essentials 05: OS Module & Environment.

---
+++ system.js
@@ -0,0 +1,9 @@
+const os = require ('os');
+
+console.log(os.userInfo());
+console.log(os.platform());
+console.log(os.release());
+console.log(os.cpus());
+console.log(__dirname);
+console.log(__filename);
+console.log(process.env);
